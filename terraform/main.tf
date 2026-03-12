b6d140b8a0361e29daf0dc7bdb32e51771948c7a
Dhruv Rupareliya <dhruvrupareliya44@gmail.com>
2026-03-12 17:53:27 +0530

    Restrict SSH access to my public IP

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,42 +1,54 @@
 provider "aws" {
- region = "ap-south-1"
+  region = "ap-south-1"
 }
 
 resource "aws_security_group" "web_sg" {
- name = "devsecops-sg"
+  name        = "devsecops-sg"
+  description = "Security group for DevSecOps project"
 
- ingress {
-   from_port = 22
-   to_port = 22
-   protocol = "tcp"
+  ingress {
+    description = "Allow SSH only from my IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
 
-   cidr_blocks = ["0.0.0.0/0"]  # Intentional vulnerability
- }
+    cidr_blocks = ["203.192.238.93/32"]
+  }
 
- ingress {
-   from_port = 3000
-   to_port = 3000
-   protocol = "tcp"
+  ingress {
+    description = "Allow web application"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
 
-   cidr_blocks = ["0.0.0.0/0"]
- }
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
- egress {
-   from_port = 0
-   to_port = 0
-   protocol = "-1"
+  egress {
+    description = "Allow HTTPS outbound"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
 
-   cidr_blocks = ["0.0.0.0/0"]
- }
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "web" {
- ami           = "ami-0e670eb768a5fc3d4"
- instance_type = "t3.micro"
+  ami           = "ami-0e670eb768a5fc3d4"
+  instance_type = "t3.micro"
 
- vpc_security_group_ids = [aws_security_group.web_sg.id]
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
 
- tags = {
-   Name = "devsecops-instance"
- }
+  metadata_options {
+    http_tokens = "required"
+  }
+
+  root_block_device {
+    encrypted = true
+  }
+
+  tags = {
+    Name = "devsecops-instance"
+  }
 }
